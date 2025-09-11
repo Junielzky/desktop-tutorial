a1578795057d01826acf2d3192df0e9cea7522d1
Junielzky <fungaming924@gmail.com>
2025-09-11 22:54:15 +0800

    Add files via upload

--- cdaApp.java
+++ cdaApp.java
@@ -11,104 +11,120 @@ public class cdaApp {
 		 String item1, item2, item3;
 		 float price1, price2, price3;
 		 float qty1, qty2, qty3;
-		 float subtotal1, subtotal2, subtotal3;
-		 float grandtotal1;
+		 float subtotal1, subtotal2;
+		 float grandtotal1, subtotal3;
 		 float change1;
-		 System.out.print("Name of Item: ");
-		 item1 = in.next();
+		System.out.print("Name of Item: ");
+		item1 = in.nextLine();
 		System.out.print("Price: ");
 		price1 = in.nextFloat();
-		if (price1 <=0) {
-		 System.out.println("ERROR!");
+		if (price1 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
 		System.out.print("Quantity: ");
 		qty1 = in.nextFloat();
-		if (qty1 <=0) {
-		 System.out.println("ERROR!");
+		if (qty1 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
 		subtotal1 = price1*qty1;
-		System.out.println("Subtotal: " + subtotal1);
-		 System.out.print("Name of Item: ");
-		 item2 = in.next();
+		System.out.println("Subtotal: ₱" + subtotal1);
+		System.out.println();
+		
+		System.out.print("Name of Item: ");
+		item2 = in.next();
 		System.out.print("Price: ");
 		price2 = in.nextFloat();
-		if (price2 <=0) {
-		 System.out.println("ERROR!");
+		if (price2 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
 		System.out.print("Quantity: ");
 		qty2 = in.nextFloat();
 		
-		if (qty2 <=0) {
-		 System.out.println("ERROR!");
+		if (qty2 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
-		subtotal2 = price2*qty2;
-		System.out.println("Subtotal: " + subtotal2);
-		 System.out.print("Name of Item: ");
-		 item3 = in.next();
+		
+		subtotal2 = price2*qty2+subtotal1;
+		System.out.println("Subtotal: ₱" + subtotal2);
+		System.out.println();
+		
+		System.out.print("Name of Item: ");
+		item3 = in.next();
 		System.out.print("Price: ");
 		price3 = in.nextFloat();
-		if (price3 <=0) {
-		 System.out.println("ERROR!");
+		if (price3 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
+		
 		System.out.print("Quantity: ");
 		qty3 = in.nextFloat();
-		if (qty3 <=0) {
-		 System.out.println("ERROR!");
+		if (qty3 <0) {
+		System.out.println("ERROR!");
 		System.exit(0);
 		}
-		subtotal3 = price3*qty3;
-		System.out.println("Subtotal: " + subtotal3);
-		grandtotal1 = subtotal1+subtotal2+subtotal3;
-		System.out.println("Grandtotal: " + grandtotal1);
-		int amount = 0, thousand, balance, fiveH, oneH, fifty, twenty, ten, five, one;
+		subtotal3 = price3*qty3+subtotal2;
+		System.out.println("Subtotal: ₱" + subtotal3);
+		System.out.println();
+		
+		grandtotal1 = subtotal3;
+		System.out.println("Grandtotal: ₱" + grandtotal1);
+		
+		int amount;
 		System.out.print ("Enter Amount: ");
 		amount = in.nextInt();
-		thousand = amount / 1000;
-		balance = amount % 1000;
-		fiveH = balance / 500;
-		balance %= 500;
-		oneH = balance / 100;
-		balance %= 100;
-		fifty = balance / 50;
-		balance = balance%= 50;
-		twenty = balance / 20;
-		balance = balance%= 20;
-		ten = balance / 10;
-		balance = balance%= 10;
-		five = balance / 5;
-		balance = balance%= 5;
-		one = balance / 1;
-		balance%= 1;
-		System.out.println ("Cash\t\tNo. of Pcs");
-		System.out.println ("1000\t\t"+thousand);
-		System.out.println ("500\t\t"+fiveH);
-		System.out.println ("100\t\t"+oneH);
-		System.out.println ("50\t\t"+fifty);
-		System.out.println ("20\t\t"+twenty);
-		System.out.println ("10\t\t"+ten);
-		System.out.println ("5\t\t"+five);
-		System.out.println ("1\t\t"+one);
-		System.out.println ("Balance: " + balance);
 		change1 = amount-grandtotal1;
-		System.out.println ("Change: " + change1);
-		if (change1 <=0) {
-	  System.out.println("ERROR!");
-		System.exit(0);
+		if (change1 <0) {
+		    System.out.println("ERROR!");
+			System.exit(0);	
 		}
+		//Cash Denomination
+		System.out.println ("Change: ₱" + change1);
+		int change2 = 0, thousand1 = 0, balance1 = 0, fiveH1 =0, oneH1 =0, fifty1 =0, twenty1 =0, ten1=0, five1=0, one1 = 0;
+		change2 = (int) change1;
+		thousand1 = change2 / 1000;
+		balance1 = change2 % 1000;
+		fiveH1 = balance1 / 500;
+		balance1 %= 500;
+		oneH1= balance1 / 100;
+		balance1 %= 100;
+		fifty1 = balance1 / 50;
+		balance1 = balance1%= 50;
+		twenty1 = balance1 / 20;
+		balance1 = balance1%= 20;
+		ten1 = balance1 / 10;
+		balance1 = balance1%= 10;
+		five1 = balance1 / 5;
+		balance1 = balance1%= 5;
+		one1 = balance1 / 1;
+		balance1%= 1;
+	System.out.println ("Cash\t\tNo. of Pcs");
+	if (thousand1 != 0) {
+    System.out.println ("1000\t\t"+thousand1);}
+	if (fiveH1 != 0) {
+	System.out.println ("500\t\t"+fiveH1); }
+    if (oneH1 != 0) {
+    System.out.println ("100\t\t"+oneH1); }
+	if (fifty1 != 0) {
+	System.out.println ("50\t\t"+fifty1); }
+	if (twenty1 != 0) {
+	System.out.println ("20\t\t"+twenty1); }
+	if (ten1 != 0) {
+	System.out.println ("10\t\t"+ten1); }
+	if (five1 != 0) {
+	System.out.println ("5\t\t"+five1); }
+	if (one1 != 0) {
+	System.out.println ("1\t\t"+one1); }
 		
 		
-		 
-		 
-		 
-		 
 		
 		
-
-	}
-
+			
+	
 }
+}
+        
